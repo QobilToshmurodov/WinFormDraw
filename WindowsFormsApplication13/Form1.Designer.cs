1f3839a85ce40d11c5852c0ca2bfffcb8e01facb
QobilToshmurodov <qobiltoshmurodov289@gmail.com>
2024-12-12 16:11:16 +0500

    first commit

---
+++ WindowsFormsApplication13/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApplication13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1297, 682);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "W - To the top";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "S - Down";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "D - To the right";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "A - To the left";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1297, 682);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keydown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication13/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        int[] mas ={0,0,0,0};
+        private void keydown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                mas[2] = mas[2] - 10;
+                g = pictureBox1.CreateGraphics();
+                Pen pen = new Pen(Color.Black, 3f);
+                Point[] points ={new Point(mas[0],mas[1]),new Point(mas[2],mas[3])};
+                g.DrawLine(pen,points[0],points[1]);
+                mas[0] = mas[2];
+                mas[1] = mas[3];
+                
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                mas[2] = mas[2] + 10;
+                g = pictureBox1.CreateGraphics();
+                Pen pen = new Pen(Color.Black, 3f);
+                Point[] points = {new Point(mas[0],mas[1]),new Point(mas[2],mas[3])};
+                g.DrawLine(pen, points[0], points[1]);
+                mas[0] = mas[2];
+                mas[1] = mas[3];
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                mas[3] = mas[3] - 10;
+                g = pictureBox1.CreateGraphics();
+                Pen pen = new Pen(Color.Black, 3f);
+                Point[] points = {new Point(mas[0],mas[1]),new Point(mas[2],mas[3])};
+                g.DrawLine(pen, points[0], points[1]);
+                mas[0] = mas[2];
+                mas[1] = mas[3];
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                mas[3]= mas[3] + 10;
+                g = pictureBox1.CreateGraphics();
+                Pen pen = new Pen(Color.Black, 3f);
+                Point[] points = { new Point(mas[0],mas[1]),new Point(mas[2],mas[3])};
+                g.DrawLine(pen, points[0], points[1]);
+                mas[0] = mas[2];
+                mas[1] = mas[3];
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
